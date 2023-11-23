4d5f8eaec5d638df96a46399ba497a4f47fcc4db
miju <alwn8918@gmail.com>
2023-11-24 07:15:54 +0900

    페이지네이션 구현

--- guide/admin.py
+++ guide/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Guide
+from .models import Guide, Category, Tag
 
 admin.site.register(Guide)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+
+
+
+
--- guide/models.py
+++ guide/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/guide/tag/{self.slug}/'
+
 class Guide(models.Model):
     title = models.CharField(max_length=50, null=True)
     period = models.CharField(max_length=50, null=True)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     image = models.URLField(null=True)
     delight_link = models.URLField(null=True)
